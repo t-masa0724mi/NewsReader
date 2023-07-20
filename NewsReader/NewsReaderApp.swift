a05f5a7fdc5ce6c232942e7446d43a76ab1cc5bb
masa950724mi <s100.iizuka.masami@gmail.com>
2023-07-20 16:03:36 +0900

    Initial Commit

---
+++ NewsReader/NewsReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsReaderApp.swift
+//  NewsReader
+//
+//  Created by 飯塚政美 on 2023/07/20.
+//
+
+import SwiftUI
+
+@main
+struct NewsReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
